fc62ead63445818ecfd337e6873196809853fe5f
Your Name <you@example.com>
2022-08-07 16:07:44 -0400

    updates on api.js

--- src/api/api.js
+++ src/api/api.js
@@ -81,9 +81,23 @@ class JoblyApi {
     return res.token;
    }
 
+   //Resgister new user(sign up)
+   static async signup(data){
+    let res = await this.request(`auth/register` , data , "post");
+    return res.token;
+   }
+
+   //Save user profile
+   static async saveProfile(username , data){
+    let res = await this.request(`users/${username}` , data , "patch");
+    return res.user;
+   }
+
 }
 
 // for now, put token ("testuser" / "password" on class)
 JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+
+export default JoblyApi;
